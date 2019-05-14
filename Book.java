312d8c53d2d7bc598a1d1b6c9f78e9b9f472e6c9
omoelyon <ganiyuelegbede@yahoo.com>
2019-05-14 16:08:31 +0100

    my first update on Book.java

--- Book.java
+++ Book.java
@@ -1,8 +1,19 @@
 public class Book{
-    private String bookName{
-        private string author ; 
+    private String bookName;
+        private String author ; 
         private boolean isHardcoover;
         private int numberOfPages;
-        
+
+        public void setAuthor(String author){
+            this.author = author;
+        public String getAuthor() {
+            return String author;
+        }
+        public void setIsHardCover() {
+            this.isHardcoover = true;
+        }
+        public boolean getIsHardCover() {
+            return isHardcover;
+        }
     }
 }
